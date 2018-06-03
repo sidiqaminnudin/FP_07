59273045ce2afc76a33091858119a6db246efbe4
sidiqaminnudin <sidiqaminnudin@students.amikom.ac.id>
2018-06-03 11:50:45 +0700

    new 1.0

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -303,7 +303,6 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   Carousel.VERSION  = '3.2.0'
 
   Carousel.DEFAULTS = {
-    interval: 5000,
     pause: 'hover',
     wrap: true
   }
